debf10d9e97f8bd141e37d76a51289e7a50225a3
divyansh39 <61549308+divyansh39@users.noreply.github.com>
2020-10-25 21:22:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 //for first dice
 var randomNumber=Math.floor(Math.random()*6)+1;//change dice from 1 to 6
 var randomImage="images/dice"+randomNumber+".png";//will chnge the current image of dice to a ramdom dice image
-var image1=document.querySelectorAll("img")[0];//change the first images
+var image1=document.querySelectorAll("img")[0];//change the first image
 image1.setAttribute("src",randomImage);//set the attribute of first image
 
 //for second dice
 var randomNumber2=Math.floor(Math.random()*6)+1;
 var randomImage2="images/dice"+randomNumber2+".png";
-var image2=document.querySelectorAll("img")[1];//change the second images
+var image2=document.querySelectorAll("img")[1];//change the second image
 image2.setAttribute("src",randomImage2);//set the attribute of second image
 
 //announcing the winner
